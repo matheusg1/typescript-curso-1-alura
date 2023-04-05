c070be18fb40d05df0b8f8287fdb78352db0f180
matheus.silva <tecmatheusg@hotmail.com>
2023-04-05 11:28:33 -0300

    Tipagem

--- app/App.ts
+++ app/App.ts
@@ -1,3 +1,11 @@
-import { Negociacao } from "./models/negociacao.js"
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100)
+const negociacao = new Negociacao(new Date(), 10, 100);
+const controller = new NegociacaoController;
+
+const form = document.querySelector(".form");
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    controller.adiciona()
+});
--- dist/js/App.js
+++ dist/js/App.js
@@ -1,2 +1,9 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 import { Negociacao } from "./models/negociacao.js";
 const negociacao = new Negociacao(new Date(), 10, 100);
+const controller = new NegociacaoController;
+const form = document.querySelector(".form");
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    controller.adiciona();
+});
